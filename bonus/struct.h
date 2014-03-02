bb2d6cfed5adb582f750129a021b8ac493d5b07d
fave_r <fave_r@epitech.eu>
2014-03-02 22:09:38 +0100

    rm stdbool.h

--- bonus/struct.h
+++ bonus/struct.h
@@ -1,16 +1,15 @@
 /*
 ** struct.h for struct in /home/alex-odet/work/Test_SDL
-** 
+**
 ** Made by Alexandre Odet
 ** Login   <alex-odet@epitech.net>
-** 
+**
 ** Started on  Sat Mar  1 16:54:11 2014 Alexandre Odet
-** Last update Sun Mar  2 21:26:53 2014 Alexandre Odet
+** Last update Sun Mar  2 22:09:31 2014 romaric
 */
 
 #include <SDL/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <stdbool.h>
 
 void	my_print_map();
