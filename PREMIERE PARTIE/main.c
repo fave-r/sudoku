96195398a304dfb70e348eb119397aef9b011c6b
fave_r <fave_r@epitech.eu>
2014-03-02 23:14:09 +0100

    mise au propre du depot

--- PREMIERE PARTIE/main.c
+++ PREMIERE PARTIE/main.c
@@ -5,7 +5,7 @@
 ** Login   <fave_r@epitech.net>
 **
 ** Started on  Sat Mar  1 09:34:03 2014 romaric
-** Last update Sun Mar  2 22:15:29 2014 romaric
+** Last update Sun Mar  2 22:07:02 2014 romaric
 */
 
 #include "sudoku.h"
@@ -18,22 +18,29 @@ int	main()
 
 void	my_loop(int fd)
 {
-  char	buffer[BUFF_SIZE];
-  char	*str;
-  int	*new;
-  int	i;
+  t_sud	p;
 
-  i = -1;
-  str = xmalloc(145 * sizeof(char));
-  while ((xread(fd, buffer, BUFF_SIZE)))
-    strncpy(str, buffer, strlen(buffer));
-  fill_map(str);
-  if((new = epur_str(str)))
-    if (estValide(new, 0))
-      while (++i <= 81)
+  p.i = -1;
+  p.k = -1;
+  while ((xread(fd, p.buffer, BUFF_SIZE)))
+    p.str = strdup(p.buffer);
+  p.map = xmalloc(sizeof(int *) * 9);
+  while (++p.k < 9)
+    p.map[p.k] = xmalloc(sizeof(int) * 9);
+  init_tab(p.str, p.map);
+  good(p.map, 0);
+  printf("|------------------|\n");
+  while (++p.i < 9)
+    {
+      p.x = -1;
+      printf("| ");
+      while (++p.x < 9)
 	{
-	  printf("%d\t", new[i]);
-	  if (i % 8 == 0)
-	    printf("\n");
+	  printf("%d", p.map[p.i][p.x]);
+	  if (p.x / 8 == 0)
+	    printf(" ");
 	}
+      printf("|\n");
+    }
+  printf("|------------------|\n");
 }
--- PREMIERE PARTIE/sudoku.h
+++ PREMIERE PARTIE/sudoku.h
@@ -5,7 +5,7 @@
 ** Login   <fave_r@epitech.net>
 **
 ** Started on  Fri Feb 28 20:46:48 2014 romaric
-** Last update Sun Mar  2 03:08:16 2014 romaric
+** Last update Sun Mar  2 17:31:38 2014 Alexandre Odet
 */
 
 #ifndef __SUDOKU__
@@ -21,25 +21,24 @@
 
 #define BUFF_SIZE 231
 
-typedef struct  s_word
+typedef struct  s_sud
 {
-}               t_word;
+  char	buffer[BUFF_SIZE];
+  char	*str;
+  int	i;
+  int	**map;
+  int	x;
+  int	k;
+}               t_sud;
 
 int     xopen(const char *pathname, int flags);
 ssize_t xread(int fd, void *buf, size_t count);
 void    *xmalloc(size_t n);
 void    my_loop(int fd);
-int     doble(int x, int y);
-int     online(int nbr, int *str, int x);
-int     oncolumn(int nbr, int *str, int j);
-int     onsquare(int nbr, int *str, int i, int j);
-int	good(int *str, int where);
-char	*fill_map(char *str);
-int     estValide(int *grille, int position);
-int     absentSurBloc(int k, int *grille, int i, int j);
-int     absentSurColonne(int k, int *grille, int j);
-int     absentSurLigne(int k, int *grille, int i);
-int     doble(int x, int y);
-int	*epur_str(char *str);
+int     good(int **map, int where);
+int     onsquare(int k, int **grille, int i, int j);
+int     oncolumn(int k, int **grille, int j);
+int     online(int k, int **grille, int i);
+void    init_tab(char *map, int **tab);
 
 #endif
